b4d36762893476dc84cf7b678e4bf011bf0a231d
ThiagoSSBacelli <80285341+ThiagoSSBacelli@users.noreply.github.com>
2021-05-12 14:05:24 -0300

    Add files via upload

---
+++ ecommerce/src/main/java/com/sustentaCommerce/ecommerce/security/UserDetailsMpl.java
@@ -0,0 +1,58 @@
+package com.sustentaCommerce.ecommerce.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.sustentaCommerce.ecommerce.model.Usuario;
+
+public class UserDetailsMpl implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserDetailsMpl(Usuario user) {
+		this.userName = user.getNomeCompletoUsuario();
+		this.password = user.getSenhaUsuario();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
