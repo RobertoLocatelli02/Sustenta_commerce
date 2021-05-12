93bfc42ccb942d44a374eee73daa9af2edac2008
Roberto Locatelli <80109160+RobertoLocatelli02@users.noreply.github.com>
2021-05-12 15:13:47 -0300

    Update and rename UserDetailsServiceMpl.java to UserDetailServiceImplement.java

--- ecommerce/src/main/java/com/sustentaCommerce/ecommerce/security/UserDetailServiceImplement.java
+++ ecommerce/src/main/java/com/sustentaCommerce/ecommerce/security/UserDetailServiceImplement.java
@@ -12,7 +12,7 @@ import com.sustentaCommerce.ecommerce.model.Usuario;
 import com.sustentaCommerce.ecommerce.repository.UsuarioRepository;
 
 @Service
-public class UserDetailsServiceMpl implements UserDetailsService {
+public class UserDetailServiceImplement implements UserDetailsService {
 
 	@Autowired
 	private UsuarioRepository userRepository;
@@ -21,8 +21,7 @@ public class UserDetailsServiceMpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		Optional<Usuario> user = userRepository.findByNomeCompletoUsuario(userName);
 		user.orElseThrow(() -> new UsernameNotFoundException(userName + "not found."));
-		return user.map(UserDetailsMpl::new).get();
+		return user.map(UserDetailsMpl :: new).get();
 
 	}
-
 }
