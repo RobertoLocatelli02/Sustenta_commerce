7e59ab6123e119bb4ce357198b84b0f34be7e86f
Kevellybp <81047087+Kevellybp@users.noreply.github.com>
2021-05-12 12:46:45 -0400

    Add files via upload

--- ecommerce/src/main/java/com/sustentaCommerce/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/com/sustentaCommerce/ecommerce/controller/UsuarioController.java
@@ -21,44 +21,67 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sustentaCommerce.ecommerce.model.Produtos;
 import com.sustentaCommerce.ecommerce.model.Usuario;
 import com.sustentaCommerce.ecommerce.repository.UsuarioRepository;
+import com.sustentaCommerce.ecommerce.service.UsuarioService;
 
-@RestController 
+@RestController
 @CrossOrigin("*")
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioRepository repositoryU;
-	
+	@Autowired
+	UsuarioService usuarioService;
+
 	@GetMapping
-	ResponseEntity <List<Usuario>> findAllUsusario(){
+	ResponseEntity<List<Usuario>> findAllUsusario() {
 		return ResponseEntity.ok(repositoryU.findAll());
 	}
-	
+
 	@GetMapping("/id/{idUsuario}")
-	ResponseEntity <Usuario> findAllByIdUsuario (@PathVariable Long idUsuario){
-		return repositoryU.findById(idUsuario).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	ResponseEntity<Usuario> findAllByIdUsuario(@PathVariable Long idUsuario) {
+		return repositoryU.findById(idUsuario).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	@GetMapping("/nome/{nomeCompletoUsuario}")
-	ResponseEntity <List<Usuario>> findAllByNomeCompletoUsuarioContainingIgnoreCase(@PathVariable String nomeCompletoUsuario){
+	ResponseEntity<List<Usuario>> findAllByNomeCompletoUsuarioContainingIgnoreCase(
+			@PathVariable String nomeCompletoUsuario) {
 		return ResponseEntity.ok(repositoryU.findAllByNomeCompletoUsuarioContainingIgnoreCase(nomeCompletoUsuario));
 	}
-	
+
 	@GetMapping("/email/{emailUsuario}")
-	ResponseEntity <List<Usuario>> findAllByEmailUsuarioContainingIgnoreCase(@PathVariable String emailUsuario){
+	ResponseEntity<List<Usuario>> findAllByEmailUsuarioContainingIgnoreCase(@PathVariable String emailUsuario) {
 		return ResponseEntity.ok(repositoryU.findAllByNomeCompletoUsuarioContainingIgnoreCase(emailUsuario));
 	}
-	
+
 	@GetMapping("/senha/{senhaUsuario}")
-	ResponseEntity <List<Usuario>> findAllBySenhaUsuarioContainingIgnoreCase(@PathVariable String senhaUsuario){
+	ResponseEntity<List<Usuario>> findAllBySenhaUsuarioContainingIgnoreCase(@PathVariable String senhaUsuario) {
 		return ResponseEntity.ok(repositoryU.findAllByNomeCompletoUsuarioContainingIgnoreCase(senhaUsuario));
 	}
-	@PostMapping
-	ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuarioNovo){
-		return ResponseEntity.status(HttpStatus.CREATED).body(repositoryU.save(usuarioNovo));
+
+	@GetMapping
+	ResponseEntity<List<Usuario>> getAllUsuario() {
+		List<Usuario> listaUsuarios = repositoryU.findAll();
+		if (!listaUsuarios.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.OK).body(listaUsuarios);
+		} else {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
 	}
-	
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> logar(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Optional<Usuario>> Post(@RequestBody Usuario usuarioNovo) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(usuarioNovo));
+
+	}
+
 	@PutMapping // atualizar informações de um produto
 	ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuarioAtualizado) { // end point
 		Optional<Usuario> usuarioExistente = repositoryU.findById(usuarioAtualizado.getIdUsuario());
@@ -67,22 +90,15 @@ public class UsuarioController {
 			usuarioExistente.get().setEmailUsuario(usuarioAtualizado.getEmailUsuario());
 			usuarioExistente.get().setSenhaUsuario(usuarioAtualizado.getSenhaUsuario());
 			usuarioExistente.get().setTipoPagamento(usuarioAtualizado.getTipoPagamento());
-		
+
 			return ResponseEntity.status(HttpStatus.CREATED).body(repositoryU.save(usuarioExistente.get()));
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
+
 	@DeleteMapping("/id/{idUsuario}")
-	void deleteUsuario (@PathVariable Long idUsuario){
+	void deleteUsuario(@PathVariable Long idUsuario) {
 		repositoryU.deleteById(idUsuario);
 	}
 }
-
-	
-	
-	
-
-	
-	
-	
